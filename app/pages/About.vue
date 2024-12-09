69ec9568c38e5e664c2ab61de6a4ff425192bf0b
Tim King <kingtimm@gmail.com>
2024-12-08 22:35:40 -0600

    fix: about

--- app/pages/About.vue
+++ app/pages/About.vue
@@ -7,6 +7,8 @@
     <H1 class="text-2xl font-bold">About</H1>
     <p>I hope that you can use this tool to spread holiday cheer with your loved ones.</p>
     <p>If you have feedback or questions - I'd love to hear on <ULink to="https://bsky.app/profile/timk.ing"  icon="i-simpleicons:bluesky" color="">Bluesky</ULink></p>
+    <h2>Thank you</h2>
+    <p>A special thanks to the Nuxt ecosystem for making it easy</p>
   </div>
 </template>
 
